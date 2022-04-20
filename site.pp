650e573d305431b7df53ec202fb8917ac609ab6f
JimmyThompson28 <102228705+JimmyThompson28@users.noreply.github.com>
2022-04-20 17:13:46 +0100

    Update site.pp

--- site.pp
+++ site.pp
@@ -2,6 +2,9 @@ node default {
     file {'/root/README':
         ensure => file,
         content => 'This is a readme and now it works',
-        owner   => 'root'
+        owner   => 'root',
+    }
+    file {'/root/README':
+    owner   => 'root',
     }
 }
