740df410a8bcd1ffb9ef4c4df925c16eec58217f
JimmyThompson28 <102228705+JimmyThompson28@users.noreply.github.com>
2022-04-26 13:33:49 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC71Y/0r004WmBLxK1Ctnwh0XtRwDZiNWLQi6xdAftoSILHz5ftafoqIzPcydm+89U/Tpc4nQqcUAHNCLUX6o1SSDSACMmA5bD849rdSvYZh//dV2yR0H+0MF3tFQbXe6r2O3/BCN2ndip7b0KqlYemj1UE8itU6U/e1cY+tcA7liIG55Cy79clwS+gcfydcKPGOOreDDxFHRqLoc777AWkuRmI4+OdOUeW2Ha2c+UdKNStwnE11yuDIeMnDqL6hJADtkXwbjBcv0f+fCgU3JYMWaBD21FgTEFfuAldaUNR+UlRw4DdLsc7ZjUK9g4qnuiijwXikJgLcqldquvLkS4b',
   }
 }
