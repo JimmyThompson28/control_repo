79ac0ae5871224359adffb2d3c37ba36d5c733c5
JimmyThompson28 <102228705+JimmyThompson28@users.noreply.github.com>
2022-04-29 09:37:33 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,13 +2,14 @@ class minecraft {
   file {‘/opt/minecraft’:
     ensure => directory,
   }
-  file {‘/opt/minecraft/minecraft_server.1.12.2.jar’:
+  file {‘/opt/minecraft/server.jar’:
     ensure => file,
-    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
   }
   package {‘java’:
     ensure => present,
   }
+  package {
   file {‘/opt/minecraft/eula.txt’:
     ensure => file,
     content => ‘eula=true’
