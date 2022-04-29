9573e8d0db622fc82fba07e1198555fae4b2dbd2
JimmyThompson28 <102228705+JimmyThompson28@users.noreply.github.com>
2022-04-29 11:09:14 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,7 +5,7 @@ class minecraft {
   file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
-    before => Service ['minecraft'],
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
